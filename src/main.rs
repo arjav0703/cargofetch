d5281b789817ee5d2e1a427d4e74c7688f7fb0b1
arjav0703 <arjavjain0703@gmail.com>
2025-05-24 21:43:26 +0530

    get cargo version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to rust fetch!");
+
+    let x = Command::new("cargo").arg("--version").output().expect("Cargo not found");
+    let _ver = x.stdout;
+    println!("Cargo version: {}", String::from_utf8_lossy(&_ver));
 }
