71a680330ebadd486b88983045eab8af1efe397d
arjav0703 <arjavjain0703@gmail.com>
2025-05-24 22:58:00 +0530

    refactor into funtions

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,19 @@ use std::process::Command;
 
 fn main() {
     println!("Welcome to rust fetch!");
+    let cargo_version = get_cargo_version();
+    println!("You are using: {}", cargo_version.trim());
+}
 
-    let x = Command::new("cargo").arg("--version").output().expect("Cargo not found");
-    let _ver = x.stdout;
-    println!("Cargo version: {}", String::from_utf8_lossy(&_ver));
+fn get_cargo_version() -> String {
+    let output = Command::new("cargo")
+        .arg("--version")
+        .output()
+        .expect("Failed to execute command");
+
+    if output.status.success() {
+        String::from_utf8_lossy(&output.stdout).to_string()
+    } else {
+        panic!("Command failed: {}", String::from_utf8_lossy(&output.stderr));
+    }
 }
