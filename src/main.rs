d64f9514c0adaf386128ab12db6d8b94e57ae2e0
arjav0703 <arjavjain0703@gmail.com>
2025-05-28 13:17:46 +0530

    use spawn

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use std::process::Command;
 
 fn main() {
-    println!("Welcome to rust fetch!");
-    let cargo_version = get_cargo_version();
-    println!("You are using Cargo version: {}", cargo_version.trim());
+    // println!("Welcome to rust fetch!");
+    // let cargo_version = get_cargo_version();
+    // println!("You are using Cargo version: {}", cargo_version.trim());
+    get_cargo_version();
 }
 
-fn get_cargo_version() -> String {
+fn get_cargo_version() {
     let output = Command::new("cargo")
         .arg("--version")
-        .output()
+        .spawn()
         .expect("Failed to execute command");
-
-    if output.status.success() {
-        String::from_utf8_lossy(&output.stdout).to_string()
-    } else {
-        panic!(
-            "Command failed: {}",
-            String::from_utf8_lossy(&output.stderr)
-        );
-    }
+    let res = output.wait_with_output().expect("Failed to wait on child");
+    // println!("Cargo version: {}", String::from_utf8_lossy(&res.stdout));
+    // if output.status.success() {
+        // String::from_utf8_lossy(&output.stdout).to_string()
+    // } else {
+        // panic!(
+            // "Command failed: {}",
+            // String::from_utf8_lossy(&output.stdout)
+        // );
+    // }
 }
