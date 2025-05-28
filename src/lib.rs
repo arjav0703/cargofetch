7a76bf5ec5a9f800c38e79624fc137c41f510c46
arjav0703 <arjavjain0703@gmail.com>
2025-05-28 13:41:26 +0530

    get metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
-fn get_meta() {
+use std::process::Command;
 
+pub fn get_meta() -> String {
+    let output = Command::new("cargo")
+        .arg("metadata")
+        .arg("--format-version=1")
+        .output()
+        .expect("Failed to execute command");
+    
+    // todo: create a fn for this success check as it is repeated
+    if output.status.success() {
+        String::from_utf8_lossy(&output.stdout).to_string()
+    } else {
+        panic!(
+            "Command failed: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+}
+
+pub fn get_cargo_version() -> String {
+    let output = Command::new("cargo")
+        .arg("--version")
+        .output()
+        .expect("Failed to execute command");
+    if output.status.success() {
+        String::from_utf8_lossy(&output.stdout).to_string()
+    } else {
+        panic!(
+            "Command failed: {}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use std::process::Command;
+use rust_fetch::{self, get_meta, get_cargo_version};
 
 fn main() {
-    println!("Welcome to rust fetch!");
+    let meta = get_meta();
     let cargo_version = get_cargo_version();
+
     println!("You are using Cargo version: {}", cargo_version.trim());
+    println!("Cargo metadata: {}", meta.trim());
 }
 
-fn get_cargo_version() {
-    let output = Command::new("cargo")
-        .arg("--version")
-        .output()
-        .expect("Failed to execute command");
-    if output.status.success() {
-        String::from_utf8_lossy(&output.stdout).to_string();
-    } else {
-        panic!(
-            "Command failed: {}",
-            String::from_utf8_lossy(&output.stdout)
-        );
-    }
-}
