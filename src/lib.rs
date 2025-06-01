c5bc1f90feb21a4f26297b09b9cd976b0c6e3f0e
arjav0703 <arjavjain0703@gmail.com>
2025-06-01 13:48:01 +0530

    get exact rust version

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub fn get_meta() -> String {
         .expect("Failed to execute command");
 
     // this will return a String
-    output_to_string(output)   
+    output_to_string(output)
 }
 
 pub fn get_cargo_version() -> String {
@@ -17,7 +17,12 @@ pub fn get_cargo_version() -> String {
         .output()
         .expect("Failed to execute command");
     // this will return a String
+
     output_to_string(output)
+        .split_whitespace()
+        .nth(1)
+        .unwrap_or("")
+        .to_string()
 }
 
 fn output_to_string(output: std::process::Output) -> String {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use rust_fetch::{self, get_meta, get_cargo_version};
+use rust_fetch::{self, get_cargo_version, get_meta};
 
 fn main() {
     let meta = get_meta();
     let cargo_version = get_cargo_version();
-
     println!("You are using Cargo version: {}", cargo_version.trim());
     println!("Cargo metadata: {}", meta.trim());
 }
-
