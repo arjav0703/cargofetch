dbf9e230581fb5499ca076758c6b5e1dadfaa74c
arjav0703 <arjavjain0703@gmail.com>
2025-06-02 19:52:38 +0530

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 pub mod parse;
+use which::which;
 
 fn main() {
+    if which("cargo").is_err() {
+        panic!("Application error: Cargo not found. Please install cargo and make sure it is in your PATH.");
+    }
+
     parse::get_meta();
     let cargo_version = parse::get_cargo_version();
     println!("You are using Cargo version: {}", cargo_version.trim());
