dcd2c8bbcc3ea8cab24143652a11e48b0ebb7596
arjav0703 <arjavjain0703@gmail.com>
2025-06-02 15:52:04 +0530

    add ascii art;

--- src/art.rs
+++ src/art.rs
@@ -13,4 +13,26 @@ pub fn output(package: &Package) {
             .collect::<Vec<_>>()
             .join(", ")
     );
+    print_art();
+}
+
+fn print_art() {
+    let ascii_art = r#"                 
+              R RRRRRRRR R          R
+ R RR       R RRRRRRRRRRRRR R      RR
+rR RRR    R RRRRRRRRRRRRRRRRR R   RRR R
+RRR RR   RRRRRRRRRRRRRRRRRRRRRRR  RRRRR
+ RRRRR  RRRRRRRRRRRRRRRRRRRRRRRR  RRRR
+  RRR RRRRRRRRRRRRRRRRRRRRRRRRRRRR RR
+    R  RRRRRRRRRR=  RR = RRRRRRRRRRR
+     RRRRRRRRRRRR=  RR = RRRRRRRRRR
+      RRRRRRRRRRR   RR   RRRRRRRRRR
+     RR==RRRRRRRRRRRRRRRRRRRRRR===RR
+     RR =  ==RRRRRRR  RRRRRR==  = RR
+      RR =     ===========     = RR
+       RR                        R
+        R                       R
+         R
+    "#;
+    println!("{}", ascii_art);
 }
