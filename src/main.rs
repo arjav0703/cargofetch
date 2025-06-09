7e4c58cab609f6f0be21813720f5fc29107709c1
arjav0703 <arjavjain0703@gmail.com>
2025-06-09 20:10:24 +0530

    implement code to get total lines in src code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 pub mod parse;
+use size::get_lines;
 use which::which;
-
+mod size;
 fn main() {
     if which("cargo").is_err() {
         panic!("Application error: Cargo not found. Please install cargo and make sure it is in your PATH.");
     }
+    let l = get_lines();
+    println!("Total lines of code in src: {}", l);
 
     parse::init();
 }
