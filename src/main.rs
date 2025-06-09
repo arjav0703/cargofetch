948ebc2f53584ce78737131f48b299e62868310f
arjav0703 <arjavjain0703@gmail.com>
2025-06-09 20:51:56 +0530

    refactor checks

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
+mod checks;
 pub mod parse;
-use std::fs;
-use which::which;
 
 fn main() {
-    env_test();
-
+    checks::env_check();
     parse::init();
-}
-
-fn env_test() {
-    if which("cargo").is_err() {
-        panic!("Application error: Cargo not found. Please install cargo and make sure it is in your PATH.");
-    }
-    match fs::read("Cargo.toml") {
-        Ok(..) => {}
-        Err(..) => {
-            panic!("Rust project not found. Cargo.toml is missing");
-        }
-    }
 }
