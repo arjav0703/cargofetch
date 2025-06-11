76b2237a0252ee0bdbfe9ed77d7b9614f139b0cd
arjav0703 <arjavjain0703@gmail.com>
2025-06-11 18:34:46 +0530

    update formulae

--- Formula/cargofetch.rb
+++ Formula/cargofetch.rb
@@ -9,7 +9,7 @@ class Cargofetch < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", "cargofetch"
   end
 
   test do
