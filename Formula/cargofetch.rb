2a4daf9fe8435c057846e06f460e3c7f997efcf4
arjav0703 <arjavjain0703@gmail.com>
2025-06-11 17:47:26 +0530

    update formulae

--- Formula/cargofetch.rb
+++ Formula/cargofetch.rb
@@ -4,17 +4,16 @@ class Cargofetch < Formula
   url "https://github.com/arjav0703/cargofetch/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "2af236d6598ea22491e654565d66d561680a3cd53c59d2d8de2eac35c39a4e7c"
   license "MIT"
+  head "https://github.com/arjav0703/cargofetch.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release"
-    bin.install "target/release/cargofetch"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
     assert_match "cargofetch 1.0.0", shell_output("#{bin}/cargofetch --version")
   end
 end
-
 
